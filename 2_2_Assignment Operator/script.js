1bde00a323927ed8ecaa5882034a22829f1cf79e
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-08-01 11:54:58 +0530

    Assignment Operator

---
+++ 2_2_Assignment Operator/script.js
@@ -0,0 +1,9 @@
+console.log("\n\n----- Assignement Operators -----\n");
+let a=5;
+
+console.log(" a+=1 : ",a+=2);
+console.log(" a-=1 : ",a-=2);
+console.log(" a*=1 : ",a*=2);
+console.log(" a/=1 : ",a/=2);
+console.log(" a**=1 : ",a**=2);
+console.log(" a%=1 : ",a%=2);
