b37ba0755be8fc48c2d92182feb14f016f23116c
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-08-01 11:46:40 +0530

    Arithmetic Operators

---
+++ 2_1_Arithemetic Operator/script.js
@@ -0,0 +1,17 @@
+let a=5;
+let b=4;
+
+console.log("\n\n----- Arithmetic Operator -----\n");
+console.log("a + b : ",(a+b));
+console.log("a - b : ",(a-b));
+console.log("a * b : ",(a*b));
+console.log("a / b : ",(a/b));
+console.log("a ** b : ",(a**b));
+console.log("a % b : ",(a%b));
+console.log("a++ : ",a++);
+a=5;
+console.log("++a : ",++a);
+a=5;
+console.log("a-- : ",a--);
+a=5;
+console.log("--a : ",--a);
