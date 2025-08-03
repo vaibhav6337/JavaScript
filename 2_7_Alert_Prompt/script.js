6aad303daac4614a8966f8a3186f287ebe1adf50
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-08-03 19:30:52 +0530

    Alert Prompt

---
+++ 2_7_Alert_Prompt/script.js
@@ -0,0 +1,12 @@
+
+alert("Hello");  //One Time POP-UP
+
+let num=prompt("Hello");
+
+console.log(num);
+
+if(num%5===0){
+    console.log(num," is multiple of 5");
+}else{
+    console.log(num," is not in the multiple of 5");
+}
