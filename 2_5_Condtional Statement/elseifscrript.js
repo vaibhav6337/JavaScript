6eca5dfa499ab16bb791782e62662b3c33f33c4c
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-08-03 19:30:04 +0530

    Conditional Statement

---
+++ 2_5_Condtional Statement/elseifscrript.js
@@ -0,0 +1,21 @@
+let a=5;
+
+if(a==1){
+    console.log("a is 1");
+}
+else if(a==2){
+    console.log("a is 2");
+
+}
+else if(a==3){
+    console.log("a is 3");
+}
+else if(a==4){
+    console.log("a is 4");
+}
+else if(a==5){
+    console.log("a is 5");
+}
+else{
+    console.log("a is not in 1 to 5");
+}
---
+++ 2_5_Condtional Statement/ifelsescript.js
@@ -0,0 +1,17 @@
+let age=17;
+if(age>=18){
+    console.log("You are eligible for voting.......");
+}
+else{
+    console.log("You are not eligible for voting");
+}
+
+let mode="dark";
+let colour;
+if(mode=="dark"){
+    colour="black";
+}
+else{
+    colour="white";
+}
+console.log(colour)
