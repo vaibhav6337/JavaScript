45ff35597f4f4d4a9da9c2123a9b15b28dcb9670
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-08-11 18:35:28 +0530

    For in loop

---
+++ 3_6_forinloop/script.js
@@ -0,0 +1,11 @@
+let student={
+    Name:"Vaibhav Bhagat",
+    Marks:95,
+    class:"A",
+    Div:"D",
+    age:20
+};
+
+for(let key in student){
+    console.log("Key : ",key," Value : ",student[key]);
+}
