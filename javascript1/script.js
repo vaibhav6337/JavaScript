606884cce64d1b3ea34bbbd323379b5001492b61
vaibhav6337 <140892436+vaibhav6337@users.noreply.github.com>
2025-08-14 18:39:27 +0530

    1st javascript program

---
+++ javascript1/script.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+
+console.log("Console is running........");
+console.log("Code is looking like a WOW !!!");
+console.log("Hii How are you ????");
+
+var a=prompt("Enter the number : ")
+var istrue=confirm("Are you sure to leave this page and burn your computer?? ");
+
+if(istrue){
+    console.log("Comptuer is blasting....");
+}else{
+    console.log("Your computer is not blasting");
+}
+console.log("Your number is  "+a);
