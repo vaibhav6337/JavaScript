9349c3f72ad36686cc63b2f52535737c93f0b9e3
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2025-08-17 16:54:01 +0530

    index file

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Hello yes")
+
+let age=17;
+let addage=1
+let grace= 2;
+age+=addage;
+console.log(age);
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age ** grace);
+console.log(age%grace);
+if((age+grace)>18){
+    console.log("You can drive !!");
+}
+else{
+    console.log("You cant drive !!");
+}
+
+if(grace==2){
+    console.log("Grace is 2");
+}
+else{
+    console.log("Grace is not 2")
+}
