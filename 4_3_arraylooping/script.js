97e74f2ce798e1fa0754ef7d5827f5687e27413c
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2025-09-12 18:19:10 +0530

    Loops in Array

---
+++ 4_3_arraylooping/script.js
@@ -0,0 +1,13 @@
+let heroes=["Shah Rukh","Salman","Tiger","Akshay","Naga Chaitanya"];
+
+//For Loop
+// for(let idx=0;idx<heroes.length;idx++){
+//     console.log(heroes[idx]);
+// }
+
+//For Of Loop
+
+for(let hero of heroes){
+    console.log(hero.toUpperCase());
+}
+
