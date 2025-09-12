795aafa35f8f0f3b5839beb8b992f82a978b666b
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2025-09-12 14:41:55 +0530

    Array 1

---
+++ 4_1_array1/script.js
@@ -0,0 +1,10 @@
+let marks=[27, 59, 76, 34, 23];
+console.log(marks);
+console.log(marks.length);
+
+let heroes=["Shahrukh","Salman", "Tiger", "Ranbir"];
+console.log(heroes);
+console.log(heroes.length);
+
+console.log(typeof marks);
+
