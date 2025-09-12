91d44f7cc62298d44be5f4af352ff38417ba892d
vaibhav bhagat <140892436+vaibhav6337@users.noreply.github.com>
2025-09-12 14:47:52 +0530

    Array Indices

---
+++ 4_2_arrayIndices/script.js
@@ -0,0 +1,14 @@
+let arr=[97, 56, 34, 90, 44];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[45]);
+
+arr[45]=455;
+console.log(arr);
+
+arr[2]=566;
+console.log(arr);
